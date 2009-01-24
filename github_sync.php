106ff1ee44c322ce894d731e8a1f88cb5f37d2cd
宣 言 <xuanyan1983@gmail.com>
2009-01-24 17:18:36 +0800

    . typo

--- github_sync.php
+++ github_sync.php
@@ -2,23 +2,10 @@
 
 /* www.kukufun.com github_sync by xuanyan <xunayan1983@gmail.com> */
 
-
 error_reporting(E_ALL);
 
 require_once './GithubSync.php';
 
-if (get_magic_quotes_gpc())
-{
-    $_POST['payload'] = stripslashes($_POST['payload']);
-}
-$array = @json_decode($_POST['payload'], true);
-
-file_put_contents('./github_in.text', var_export($array, true));
-
-
 GithubSync::Start('git://github.com/xuanyan/github_sync.git', dirname(__FILE__));
-
-exit;
-
 
 ?>
