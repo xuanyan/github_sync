84c2b8b65361e957dac59c354ac4f48ce0cb4c9d
宣 言 <xuanyan1983@gmail.com>
2009-01-24 12:15:35 +0800

    . text

--- github_sync.php
+++ github_sync.php
@@ -6,6 +6,9 @@
 error_reporting(E_ALL);
 set_time_limit(0);
 require_once './GithubSync.php';
+
+file_put_contents(var_export($_POST, true));
+
 GithubSync::Start('git://github.com/xuanyan/github_sync.git', dirname(__FILE__));
 
 exit;
